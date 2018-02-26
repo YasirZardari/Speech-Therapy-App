d25e89720a0a794587022f5765f0577d9a1c77d4
YasirZardari <yasirzardari@gmail.com>
2018-02-26 19:09:08 +0000

    Updated Main Menu

--- SpeechTherapy/app/screens/MainMenu.js
+++ SpeechTherapy/app/screens/MainMenu.js
@@ -3,8 +3,11 @@ import {
   StyleSheet,
   View,
   Text,
+  Dimensions, 
+  Platform,
   TouchableOpacity
 } from 'react-native';
+
 
 type Props = {};
 class MainMenu extends Component<Props> {
@@ -18,20 +21,34 @@ class MainMenu extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Welcome to MainMenu</Text>
+        {/* <Text>Welcome to MainMenu</Text> */}
 
         <TouchableOpacity
           style={styles.button}
           onPress={this.onPressRecord}
         >
-          <Text>Record Audio</Text>
+          <Text style={styles.buttonText}>Record Audio</Text>
         </TouchableOpacity>
 
         <TouchableOpacity
-          style={styles.button}
+          style={styles.button2}
           onPress={this.onPressCategories}
         >
-          <Text>Categories</Text>
+          <Text style={styles.buttonText}>Categories</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.button3}
+          onPress={this.onPressCategories}
+        >
+          <Text style={styles.buttonText}>Favourites</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.button4}
+          onPress={this.onPressCategories}
+        >
+          <Text style={styles.buttonText}>Text-to-Speech</Text>
         </TouchableOpacity>
 
       </View>
@@ -48,8 +65,48 @@ const styles = StyleSheet.create({
     backgroundColor: '#F5FCFF',
   },
   button: {
-    alignItems: 'center',
-    backgroundColor: '#DDDDDD',
-    padding: 10
+    //alignItems: 'center',
+    backgroundColor: '#007aff',
+    position: 'absolute',
+    top:50,
+    left: (Dimensions.get('window').width / 2) - 150,
+    padding: 10,
+    width: 300, 
+    height: 80
+  },
+  button2: {
+    //alignItems: 'center',
+    position: 'absolute',
+    top:170,
+    left: (Dimensions.get('window').width / 2) - 150,
+    backgroundColor: '#007aff',
+    padding: 10,
+    width: 300, 
+    height: 80
+  },
+  button3: {
+    position: 'absolute',
+    top:290,
+    left: (Dimensions.get('window').width / 2) - 150,
+    backgroundColor: '#007aff',
+    padding: 10,
+    width: 300, 
+    height: 80
+  },
+  button4: {
+    position: 'absolute',
+    top:410,
+    left: (Dimensions.get('window').width / 2) - 150,
+    backgroundColor: '#007aff',
+    padding: 10,
+    width: 300, 
+    height: 80
+  },
+  buttonText: {
+    padding: 20,
+    fontSize: 25,
+    textAlign: 'center',
+    justifyContent: 'center',
+    color: 'white'
   }
 });
