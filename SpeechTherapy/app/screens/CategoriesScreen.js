392600fe60b0995fdcbeaa19423a3c474aafdb17
bharrisotcd <36005478+bharrisotcd@users.noreply.github.com>
2018-03-05 22:33:14 +0000

    Update CategoriesScreen.js

--- SpeechTherapy/app/screens/CategoriesScreen.js
+++ SpeechTherapy/app/screens/CategoriesScreen.js
@@ -3,15 +3,53 @@ import {
   StyleSheet,
   View,
   Text,
+  Dimensions,
+  Platform,
   TouchableOpacity
 } from 'react-native';
 
 type Props = {};
 class CategoriesScreen extends Component<Props> {
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Welcome to Categories Screen</Text>
+      <Text style={styles.headingText}>Categories</Text>
+      <TouchableOpacity
+      style={styles.button}
+      onPress={this.onPressCategory}
+      >
+      <Text style={styles.buttonText}>Category1</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+      style={styles.button2}
+      onPress={this.onPressCategory}
+      >
+      <Text style={styles.buttonText}>Category2</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+      style={styles.button3}
+      onPress={this.onPressCategory}
+      >
+      <Text style={styles.buttonText}>Category3</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+      style={styles.button4}
+      onPress={this.onPressCategory}
+      >
+      <Text style={styles.buttonText}>Category4</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+      style={styles.button5}
+      onPress={this.onPressCategory}
+      >
+      <Text style={styles.buttonText}>Category5</Text>
+      </TouchableOpacity>
+
       </View>
     );
   }
@@ -24,5 +62,67 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
+  },
+  button: {
+    //alignItems: 'center',
+    backgroundColor: '#336699',
+    position: 'absolute',
+    top:100,
+    left: (Dimensions.get('window').width / 2) - 150,
+    padding: 10,
+    width: 300,
+    height: 80
+  },
+  button2: {
+    //alignItems: 'center',
+    position: 'absolute',
+    top:200,
+    left: (Dimensions.get('window').width / 2) - 150,
+    backgroundColor: '#336699',
+    padding: 10,
+    width: 300,
+    height: 80
+  },
+  button3: {
+    position: 'absolute',
+    top:300,
+    left: (Dimensions.get('window').width / 2) - 150,
+    backgroundColor: '#336699',
+    padding: 10,
+    width: 300,
+    height: 80
+  },
+  button4: {
+    position: 'absolute',
+    top:400,
+    left: (Dimensions.get('window').width / 2) - 150,
+    backgroundColor: '#336699',
+    padding: 10,
+    width: 300,
+    height: 80
+  },
+  button5: {
+    position: 'absolute',
+    top:500,
+    left: (Dimensions.get('window').width / 2) - 150,
+    backgroundColor: '#336699',
+    padding: 10,
+    width: 300,
+    height: 80
+  },
+  buttonText: {
+    padding: 20,
+    fontSize: 25,
+    textAlign: 'center',
+    justifyContent: 'center',
+    color: 'white'
+  },
+  headingText: {
+    position: 'absolute',
+    fontSize: 25,
+    top: 30,
+    textAlign: 'center',
+    justifyContent: 'center',
+    color: 'grey'
   }
 });
