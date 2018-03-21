814efe638ff746882942c2593acec98430cd1bd4
saphilli <saphilli@tcd.ie>
2018-03-21 01:31:35 +0000

    Add files via upload

--- SpeechTherapy/app/config/router.js
+++ SpeechTherapy/app/config/router.js
@@ -3,6 +3,9 @@ import { StackNavigator } from 'react-navigation';
 import MainMenu from '../screens/MainMenu';
 import RecordScreen from '../screens/RecordScreen';
 import CategoriesScreen from '../screens/CategoriesScreen';
+import CategoryScreen from '../screens/CategoryScreen';
+import FavouritesScreen from '../screens/FavouritesScreen';
+import SaveRecordingScreen from '../screens/SaveRecordingScreen';
 
 export const Root = StackNavigator({
   MainMenu: {
@@ -13,6 +16,15 @@ export const Root = StackNavigator({
   },
   CategoriesScreen: {
     screen: CategoriesScreen
+  },
+  CategoryScreen: {
+    screen: CategoryScreen
+  },
+  FavouritesScreen: {
+    screen: FavouritesScreen
+  },
+  SaveRecordingScreen: {
+   screen: SaveRecordingScreen
   }
 });
 
