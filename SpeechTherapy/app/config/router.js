3bf9629925eb52f2999f48d809c6864a0bb9952d
saphilli <saphilli@tcd.ie>
2018-03-25 17:30:42 +0100

    Changes to nav bar + added Favourites Screen

--- SpeechTherapy/app/config/router.js
+++ SpeechTherapy/app/config/router.js
@@ -6,28 +6,70 @@ import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryScreen from '../screens/CategoryScreen';
 import SaveRecordingScreen from '../screens/SaveRecordingScreen';
 import TTSScreen from '../screens/TTSScreen';
+import FavouritesScreen from '../screens/FavouritesScreen';
 
 export const Root = StackNavigator({
   MainMenu: {
-    screen: MainMenu
+    screen: MainMenu,
+    navigationOptions: {
+      title: 'Message Banking',
+      headerStyle: { backgroundColor: '#52b2d8' },
+      headerTitleStyle: {color: '#FFFFFF' },
+      headerTintColor: '#FFFFFF',
+      fontFamily:'sans-serif-condensed'
+    }
   },
   RecordScreen: {
-    screen: RecordScreen
+    screen: RecordScreen,
+    navigationOptions: {
+      title: 'Make a Recording',
+      headerStyle: { backgroundColor: '#52b2d8' },
+      headerTitleStyle: { color: '#FFFFFF' },
+      headerTintColor: '#FFFFFF',
+      fontFamily:'sans-serif-condensed'
+    }
   },
   CategoriesScreen: {
-    screen: CategoriesScreen
+    screen: CategoriesScreen,
+    navigationOptions: {
+      title: 'Categories',
+      headerStyle: { backgroundColor: '#52b2d8' },
+      headerTitleStyle: { color: '#FFFFFF' },
+      headerTintColor: '#FFFFFF',
+      fontFamily:'sans-serif-condensed'
+    }
   },
   CategoryScreen: {
-    screen: CategoryScreen
+    screen: CategoryScreen,
+    navigationOptions: {
+      headerStyle: { backgroundColor: '#52b2d8' },
+      headerTitleStyle: { color: '#FFFFFF' },
+      headerTintColor: '#FFFFFF',
+      fontFamily:'sans-serif-condensed'
+    }
+  },
+  FavouritesScreen: {
+    screen: FavouritesScreen,
+    navigationOptions: {
+      title:'Favourites',
+      headerStyle: { backgroundColor: '#52b2d8' },
+      headerTitleStyle: { color: '#FFFFFF' },
+      headerTintColor: '#FFFFFF',
+      fontFamily:'sans-serif-condensed'
+    }
   },
   SaveRecordingScreen: {
     screen: SaveRecordingScreen,
     navigationOptions: {
       title: 'Save Recording',
-      headerStyle: { backgroundColor: '#03A9F4' },
+      headerStyle: { backgroundColor: '#52b2d8' },
       headerTitleStyle: { color: '#FFFFFF' },
       headerTintColor: '#FFFFFF',
+      fontFamily:'sans-serif-condensed'
     }
+  },
+  TTSScreen: {
+    screen: TTSScreen
   }
 });
 
