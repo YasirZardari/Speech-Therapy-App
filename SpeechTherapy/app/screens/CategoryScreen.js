b193e4eda03bc62aa13ac0af8382f2b00b9c38f7
saphilli <saphilli@tcd.ie>
2018-03-26 21:25:15 +0100

    Update CategoryScreen.js

--- SpeechTherapy/app/screens/CategoryScreen.js
+++ SpeechTherapy/app/screens/CategoryScreen.js
@@ -8,30 +8,20 @@ import {
     Dimensions,
     TextInput,
     Button,
+    NativeModules,
     TouchableOpacity,
     TouchableHighlight,
     ToastAndroid
 } from 'react-native';
 import { List, ListItem} from 'react-native-elements';
+import Icon from 'react-native-vector-icons/EvilIcons';
 
-var RecordingArray = ["Recording",
-"Recording",
-"Recording",
-"Recording",
-"Recording",
-"Recording"];
+var RecordingArray = ["How are you?",
+"Nice to see you",
+"Are you doing well?",
+"My Name",
+"Hello"];
 
-deleteCategory=(stringToDelete)=>{
-  Alert.alert(
-    "Warning",
-    "Are you sure you want to delete this category?",
-    [
-      {text: "Cancel",onPress:() => console.log('Cancel Pressed'),
-      style:'cancel'},
-      {text: "OK",onPress:() => {this.RemoveItemFromArray}}
-    ], {cancelable:false}
-  );
-}
 
 class CategoryScreen extends Component<Props> {
 
@@ -39,11 +29,34 @@ class CategoryScreen extends Component<Props> {
     super(props)
     this.state = {temp: ''}
   }
+  RemoveItemFromArray=(itemToDelete)=>{
+    for (var i=RecordingArray.length-1; i>=0; i--) {
+      if (RecordingArray[i] === itemToDelete) {
+        RecordingArray.splice(i, 1);
+      }
+    }
+    this.setState({RecordingArray});
+  }
+  removeRecording=(stringToDelete)=>{
+    Alert.alert(
+      "Warning",
+      "Are you sure you want to remove "+ stringToDelete + " from this category?",
+      [
+        {text: "Cancel",onPress:() => console.log('Cancel Pressed'),
+        style:'cancel'},
+        {text: "OK",onPress:() => //fileManager.deleteCategory(stringToDelete)}
+        {this.RemoveItemFromArray(stringToDelete)}}
+      ], {cancelable:false}
+    );
+  }
+
   render(){
     return(
       <List containerStyle = {{
         marginTop:0,
-       marginBottom:80}}>
+       marginBottom:80,
+       borderTopWidth:0,
+       borderBottomWidth:0}}>
       <FlatList
         data = {RecordingArray}
         extraData={this.state}
@@ -52,20 +65,16 @@ class CategoryScreen extends Component<Props> {
         renderItem={({item}) => {
           return (
           <ListItem
-               // <TouchableHighlight
-               //  onPress={() => this.onPressCategory(item)}>
-               //   <Text style={styles.categoryText}>{item}</Text>
-               // </TouchableHighlight>
               title = {item}
-              titleStyle = {{fontSize:25}}
+              titleStyle = {styles.recordingText}
               rightIcon = {
                 <Icon
-                  name="trash"
+                  name="minus"
                   size={40}
-                  onPress= {this.deleteCategory}
+                  onPress= {() =>this.removeRecording(item)}
                 />
               }
-              containerStyle = {{borderBottomWidth :1},{height:80}}
+              containerStyle = {styles.container}
               />
             )
         }}
@@ -75,3 +84,16 @@ class CategoryScreen extends Component<Props> {
   }
 }
 export default CategoryScreen;
+const styles = StyleSheet.create({
+  container: {
+    borderBottomWidth :1,
+    height:80
+  },
+  recordingText: {
+    fontSize:23,
+    padding:22,
+    textAlign:'left',
+    fontFamily: 'sans-serif-condensed'
+
+  }
+});
