ba52b54b3877c3fb0a11c6dad8adb52c7ea40b7b
saphilli <saphilli@tcd.ie>
2018-04-01 20:38:11 +0100

    scaling UI

--- SpeechTherapy/app/screens/RecordScreen.js
+++ SpeechTherapy/app/screens/RecordScreen.js
@@ -3,7 +3,6 @@ import {
   StyleSheet,
   View,
   Text,
-  TextInput,
   TouchableOpacity,
   NativeModules,
   ToastAndroid,
@@ -14,7 +13,6 @@ import Pulse from 'react-native-pulse'; //npm install react-native-pulse --save
 
 const WavAudioRecord = NativeModules.WavAudioRecord;
 
-type Props = {};
 class RecordScreen extends Component<Props> {
   constructor(props) {
     super(props);
@@ -28,20 +26,19 @@ class RecordScreen extends Component<Props> {
   }
 
   componentWillMount() {
-    if (!this.state.hasPermission) {
-      WavAudioRecord.checkAuthorisation().then(function(hasPermission) {
-        if (!hasPermission) {
-          ToastAndroid.show('No Mic Permission', ToastAndroid.SHORT);
-        }
-      });
-      this.setState({ hasPermission: true });
-    }
+     if (!this.state.hasPermission) {
+       WavAudioRecord.checkAuthorisation().then(function(hasPermission) {
+         if (!hasPermission) {
+           ToastAndroid.show('No Mic Permission', ToastAndroid.SHORT);
+         }
+       });
+       this.setState({ hasPermission: true });
+     }
   }
 
   onPressRecord = () => {
     if (!this.state.isRecording) {
-      // start recording
-
+    //  start recording
       this.setState({pulse:true})
       WavAudioRecord.startRecording();
       this.setState({ isRecording: true, buttonText: 'Stop Recording',
@@ -56,54 +53,71 @@ class RecordScreen extends Component<Props> {
 
   render() {
     return (
-      <View style={styles.container}>{
-        this.state.pulse ? <Pulse
-          color ='skyblue'
-          numPulses={10}
-          diameter={350}
+      <View style={styles.container}>
+         <View style={styles.topContainer}>
+          <View style={styles.pulseContainer}>{
+            this.state.pulse ?
+                <Pulse
+                  color ='skyblue'
+                  numPulses={10}
+                  diameter={350}
+                  speed={20}
+                  duration ={1000}
+                />
+              :null}
+            <View style={styles.iconContainer}>
+              <Icon
+              name= {this.state.micIcon}
+              size={100}
+              />
+            </View>
+          </View>
 
-          speed={20}
-
-          duration ={1000}
-          style= {{top:40, justifyContent:'center'}}/>: null
-      }
-        <View style={styles.buttonContainer}>
-        <Icon
-          name= {this.state.micIcon}
-          size={100}
-          style = {{top:150,left:(Dimensions.get('window').width / 2)-97
-        }}
-        />
-          <TouchableOpacity style={[styles.button,{top:290},
-            {justifyContent:'center'},{width: 320},
-             this.state.isRecording && styles.buttonOnRec]}
-            onPress={this.onPressRecord}>
-            <Text style={styles.buttonText}>{ this.state.buttonText }</Text>
-          </TouchableOpacity>
-        </View>
+         </View>
+      <View style = {styles.buttonContainer}>
+        <TouchableOpacity style={[styles.button,{width: 320},
+          this.state.isRecording && styles.buttonOnRec]}
+          onPress={this.onPressRecord}>
+          <Text style={styles.buttonText}>{ this.state.buttonText }</Text>
+        </TouchableOpacity>
       </View>
+     </View>
     );
   }
 }
 export default RecordScreen;
-
 const styles = StyleSheet.create({
-  container: {
+  container:{
     flex: 1,
+    flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF'
   },
+  topContainer :{
+    flex:2,
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  iconContainer: {
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  pulseContainer: {
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
   buttonContainer: {
-    flex: 1,
-    justifyContent: 'flex-start',
+    flex: 2,
+    justifyContent: 'center',
     paddingTop: 10
   },
   button: {
+    justifyContent: 'center',
     height: 100,
     alignItems: 'center',
     backgroundColor: '#52b2d8',
-    top: 290,
     borderRadius: 10,
     elevation: 6
   },
@@ -114,17 +128,17 @@ const styles = StyleSheet.create({
     fontSize: 30,
     fontFamily:'sans-serif-condensed',
     color: 'white'
-  },
-  textInputContainer: {
-    flex: 1,
-    justifyContent: 'flex-end',
-    paddingBottom: 10,
-    fontFamily:'sans-serif-condensed'
-  },
-  textInput: {
-    height: 40,
-    width: 320,
-    fontSize: 24,
-    fontFamily:'sans-serif-condensed'
   }
+  // textInputContainer: {
+  //   flex: 1,
+  //   justifyContent: 'flex-end',
+  //   paddingBottom: 10,
+  //   fontFamily:'sans-serif-condensed'
+  // },
+  // textInput: {
+  //   height: 40,
+  //   width: 320,
+  //   fontSize: 24,
+  //   fontFamily:'sans-serif-condensed'
+  // }
 });
